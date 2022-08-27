feccbec15f12ee9355cfadce3fc39cd6c1dfa075
Braulio De La Garza <brauliodelagarza11@gmail.com>
2022-08-26 19:45:35 -0500

    Add cities service basic functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/cities.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, take } from 'rxjs/operators';
+import { CitiesResponseModel } from '../shared/models/cities-response.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CitiesService {
+
+  constructor(private http: HttpClient) { }
+
+  public getCityDetails(): Observable<CitiesResponseModel> { // Expect response model
+    return this.http.get<CitiesResponseModel>('https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q60', { // API URL
+      headers: new HttpHeaders({
+        'X-RapidAPI-Key': 'e6c33ffd35msh7138885537fe8cbp1b1f5bjsne18c7cb4a25b',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+      })
+    }).pipe(take(1));
+  }
+}
