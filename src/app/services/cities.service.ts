a0e18a6bd3e47360fa558f553fd85e476efa7d5f
Braulio De La Garza <brauliodelagarza11@gmail.com>
2022-08-31 19:34:39 -0500

    Update cities service

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
@@ -18,6 +18,16 @@ export class CitiesService {
         'X-RapidAPI-Key': 'e6c33ffd35msh7138885537fe8cbp1b1f5bjsne18c7cb4a25b',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
       })
+    }).pipe(take(1));
+  }
+
+  public getCityDescription(city: string): Observable<any> {
+    return this.http.get<any>('https://wiki-briefs.p.rapidapi.com/search', { // API URL
+      headers: new HttpHeaders({
+        'X-RapidAPI-Key': '5b8b53d7f6mshb64105ad229cf77p1c7bd2jsn08db3a0517fe',
+        'X-RapidAPI-Host': 'wiki-briefs.p.rapidapi.com'
+      }),
+      params: new HttpParams().set('q', city)
     }).pipe(take(1));
   }
 }
